1d77b21078d2252ff91d6105c37107cd4e10365a
Ricardo <ricardog.navi20@gmail.com>
2019-11-08 09:24:15 -0600

    second happy commit

--- GreenBoyWeb/settings.py
+++ GreenBoyWeb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'GreenBoyWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BeastBoy',
+        'USER': 'postgres',
+        'PASSWORD': 'Naruto20!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
