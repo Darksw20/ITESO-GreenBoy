40638a025b3cf6df68e1f9880fbc95c882b9dc80
Ricardo <ricardog.navi20@gmail.com>
2019-11-08 13:41:33 -0600

    new commit

--- GreenBoyWeb/settings.py
+++ GreenBoyWeb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'greenBoy.apps.GreenboyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
